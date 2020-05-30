b9f82c48aeb2f674072313ef9f07a9d88b485e06
guohang <2113212723@qq.com>
2020-05-30 18:40:10 +0800

    优化部分代码

---
+++ thethird/library.java
@@ -0,0 +1,40 @@
+package thethird;
+
+
+class library {
+    // 初始化总数为1
+    private int sum = 1;
+
+    // 设置锁, 一个对象操作一次
+    public synchronized void add() {
+        if (sum < 3) {
+            sum++;
+            System.out.println("还书完成,现在还有" + sum + "本书");
+        } else {
+            System.out.println("书已满!");
+        }
+        try {
+        // 抓异常
+            Thread.sleep(100);
+        } catch (Exception ignore) {
+        }
+    }
+
+    // 设置锁
+    public synchronized void reduce() throws InterruptedException {
+        if (sum > 0) {
+            sum--;
+            System.out.println("借书完成, 现在还有" + sum + "本书");
+        } else {
+            System.out.println("书空了!");
+            wait();
+            notify();
+        }
+        try {
+        // 抓异常
+            Thread.sleep(100);
+        } catch (Exception ignore) {
+        }
+
+    }
+}
---
+++ thethird/test.java
@@ -0,0 +1,22 @@
+package thethird;
+
+public class test {
+    public static void main(String[] args) {
+        // 创建一个图书馆类
+        library book = new library();
+
+        // 创建俩个操作类对象, 分别是存钱与 取钱
+        operation save = new operation(book, operation.TYPE_ADD);
+        // 取钱类
+        operation draw = new operation(book, operation.TYPE_REDUCE);
+
+        // 创建俩个线程并传入俩个操作类
+        Thread t1 = new Thread(save);
+        Thread t2 = new Thread(draw);
+
+        // 线程开始
+        t1.start();
+        t2.start();
+
+    }
+}
